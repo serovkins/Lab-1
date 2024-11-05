d5d578625a02a90a3725d24c16f255c37eb28644
serovkins <serovivashka@yandex.ru>
2024-11-05 18:10:42 +0300

    Update diagrama.py

--- diagrama.py
+++ diagrama.py
@@ -1,22 +1,22 @@
 with open('sequence.txt', 'r') as f:
     all_numbers = [num.strip() for num in f]
 
-nechet = [v for k, v in enumerate(all_numbers) if not k % 2]
-chet = [v for k, v in enumerate(all_numbers) if k % 2]
+odd = [v for k, v in enumerate(all_numbers) if not k % 2]
+even = [v for k, v in enumerate(all_numbers) if k % 2]
 
-nechet = [abs(float(x)) for x in nechet]
-chet = [abs(float(x)) for x in chet]
+odd = [abs(float(x)) for x in odd]
+even = [abs(float(x)) for x in even]
 a = [abs(float(x)) for x in all_numbers]
 
-average_chet = sum(chet) / len(chet) if chet else 0
-average_nechet = sum(nechet) / len(nechet) if nechet else 0
+average_even = sum(even) / len(even) if even else 0
+average_odd = sum(odd) / len(odd) if odd else 0
 average_all = sum(a) / len(a) if a else 0
 
-otnoshenie_chet_k_all = average_chet / average_all
-otnoshenie_nechet_k_all = average_nechet / average_all
+even_k_all = average_even / average_all
+odd_k_all = average_odd / average_all
 
 
-print(f"  │ Четные:   {''.join(['\x1b[48;5;22;1m \x1b[0m' for _ in range(int(otnoshenie_chet_k_all * 40))]):40s} "
-      f"\x1b[48;5;22;1m {otnoshenie_chet_k_all * 100:.2f}% \x1b[0m │")
-print(f"  │ Нечетные: {''.join(['\x1b[48;5;46;1m \x1b[0m' for _ in range(int(otnoshenie_nechet_k_all * 40))]):40s} "
-      f"\x1b[48;5;46;1m {otnoshenie_nechet_k_all * 100:.2f}% \x1b[0m │")
+print(f"  │ Четные:   {''.join(['\x1b[48;5;22;1m \x1b[0m' for _ in range(int(even_k_all * 40))]):40s} "
+      f"\x1b[48;5;22;1m {even_k_all * 100:.2f}% \x1b[0m │")
+print(f"  │ Нечетные: {''.join(['\x1b[48;5;46;1m \x1b[0m' for _ in range(int(odd_k_all * 40))]):40s} "
+      f"\x1b[48;5;46;1m {odd_k_all * 100:.2f}% \x1b[0m │")
